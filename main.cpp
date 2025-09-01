fa26792376e5b598c4fac205ddbdc13b76e59ddb
clippy76 <clippy76@vt.edu>
2025-09-01 17:37:05 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//main.cpp
+#include <iostream>
+#include "prime.hpp"
+
+int main() {
+    constexpr int LO = 2, HI = 10000;
+    
+    // TODO: Count modulo operations for both algorithms
+    long long halfOps = countModOps(isPrimeHalf, LO, HI);
+    long long sqrtOps = countModOps(isPrimeSqrt, LO, HI);
+    
+    // TODO: Print the results
+    std::cout << "Half method operations: " << halfOps << "\n";
+    std::cout << "Sqrt method operations: " << sqrtOps << "\n";
+    
+    double ratio = halfOps/sqrtOps;
+    std::cout << "Ratio (Half/Sqrt): " << ratio << "\n";
+    
+    return 0;
+}
---
+++ prime.cpp
@@ -0,0 +1,74 @@
+//prime.cpp
+#include "prime.hpp"
+#include "cmath"
+
+bool isPrimeHalf(int n, long long* modOps) 
+{
+    long long counter = 0;
+    // Gets edge case for 0 and 1
+    if (n < 2)
+    {
+        return false;
+    }
+    // Checks for divisibility up to n/2
+    for (int i = 2; i <= n/2; i++)
+    {
+        counter++;
+        // Checks if number is prime
+        if (n % i == 0)
+        {
+            // Checks for if modOps is not nullptr
+            if (modOps)
+            {
+                *modOps += counter;
+            }
+            return false;
+        }
+    }
+    // Checks for if modOps is not nullptr
+    if (modOps)
+    {
+        *modOps += counter;
+    }
+    return true;
+}
+
+bool isPrimeSqrt(int n, long long* modOps) 
+{
+    long long counter = 0;
+    // Gets edge case for 0 and 1
+    if (n < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+                counter++;
+        // Checks if number is prime
+        if (n % i == 0)
+        {
+            // Checks for if modOps is not nullptr
+            if (modOps)
+            {
+                *modOps += counter;
+            }
+            return false;
+        }
+    }
+    // Checks for if modOps is not nullptr
+    if (modOps)
+    {
+        *modOps += counter;
+    }
+    return true;
+}
+
+long long countModOps(PrimeFn isPrime, int lo, int hi) 
+{
+    long long counter = 0;
+    for (int i = lo; i <= hi; i++)
+    {
+        isPrime(i, &counter);
+    }
+    return counter;
+}
